bc82ea3fce99090e9310ed3cbdaa19771081aebf
Akash Chowdhury <akashchowdhruy818@gmail.com>
2020-02-07 14:17:46 -0800

    Added some important features such as layout for Registration

---
+++ app/src/main/java/com/example/thinkanddo/RegisterActivity.java
@@ -0,0 +1,26 @@
+package com.example.thinkanddo;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setTitle("Create an account");
+
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
+    }
+}
