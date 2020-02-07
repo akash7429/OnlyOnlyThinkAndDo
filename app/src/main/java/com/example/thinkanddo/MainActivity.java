b02c05745f720d1be52a7bd1c145e1e1cc8b5dd3
Akash Chowdhury <akashchowdhruy818@gmail.com>
2020-02-07 13:25:36 -0800

    connwcted to firebase.

--- app/src/main/java/com/example/thinkanddo/MainActivity.java
+++ app/src/main/java/com/example/thinkanddo/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.thinkanddo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button mRegisterBtn, mLoginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRegisterBtn=findViewById(R.id.register_btn);
+        mLoginBtn=findViewById(R.id.login_btn);
+
+        mRegisterBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                //start Register Activity
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+
+            }
+        });
     }
 }
